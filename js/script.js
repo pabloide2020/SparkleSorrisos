632c97bde0be6d9bd890a1a07282c282b5465b48
Pablo de Paula <pablo270404@gmail.com>
2022-09-11 13:47:27 -0300

    mobile first/ faq styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
 const navBarLinks = document.getElementsByClassName("navbar-links")[0];
 
+const questionBox = document.getElementsByClassName("faq-question")
+console.log(questionBox)
+for (let question of questionBox) {
+  console.log(question)
+  question.addEventListener("click", () => {
+    question.classList.toggle("question-active")
+  })
+}
+
 toggleBtn.addEventListener("click", () => {
   navBarLinks.classList.toggle("active");
 });
