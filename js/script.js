5c6f739db8dd30abca83ec10da982d3d4340bf4b
Pablo de Paula <pablo270404@gmail.com>
2022-09-11 15:38:15 -0300

    mobile first complete

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
 const navBarLinks = document.getElementsByClassName("navbar-links")[0];
+const dropdownBtn = document.getElementsByClassName('show-drop')[0]
+const dropdown = document.getElementById('dropdown-list')
 
 const questionBox = document.getElementsByClassName("faq-question")
-console.log(questionBox)
+
+dropdownBtn.addEventListener('click', () => {
+  dropdown.classList.toggle('drop-active')
+  dropdownBtn.classList.toggle('drop-rotate')
+})
+
 for (let question of questionBox) {
   console.log(question)
   question.addEventListener("click", () => {
