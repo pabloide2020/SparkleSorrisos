078af0416062d68c74f6445da37a22e1b07a2b00
Pablo de Paula <pablo270404@gmail.com>
2022-09-14 19:47:03 -0300

    Suitable for normal desktops

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 const toggleBtn = document.getElementsByClassName("toggle-btn")[0];
 const navBarLinks = document.getElementsByClassName("navbar-links")[0];
-const dropdownBtn = document.getElementsByClassName('show-drop')[0]
-const dropdown = document.getElementById('dropdown-list')
+const dropdownBtn = document.getElementsByClassName("show-drop")[0];
+const dropdown = document.getElementById("dropdown-list");
 
-const questionBox = document.getElementsByClassName("faq-question")
+const questionBox = document.getElementsByClassName("faq-question");
 
-dropdownBtn.addEventListener('click', () => {
-  dropdown.classList.toggle('drop-active')
-  dropdownBtn.classList.toggle('drop-rotate')
-})
+dropdownBtn.addEventListener("click", () => {
+  dropdown.classList.toggle("drop-active");
+  dropdownBtn.classList.toggle("drop-rotate");
+});
 
 for (let question of questionBox) {
-  console.log(question)
+  console.log(question);
   question.addEventListener("click", () => {
-    question.classList.toggle("question-active")
-  })
+    question.classList.toggle("question-active");
+  });
 }
 
 toggleBtn.addEventListener("click", () => {
